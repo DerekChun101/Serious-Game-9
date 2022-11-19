7873eb09500c31d15a72474b5c51af181e969e6c
Derek Chun <derekchun2013@gmail.com>
2022-11-19 02:13:12 -0500

    updates

--- SeriousGame/Assets/EnemySpawner.cs
+++ SeriousGame/Assets/EnemySpawner.cs
@@ -7,6 +7,8 @@ public class EnemySpawner : MonoBehaviour
     public GameObject enemy;
     Vector2[] vectors = { new Vector2(3, 2.33f), new Vector2(5, 2.33f), new Vector2(7, 2.33f) };
     public int Num_Enemy;
+   
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +26,7 @@ public class EnemySpawner : MonoBehaviour
         for (int i = 1; i <= Num_Enemy; i++)
         {
             GameObject new_Enemy = Instantiate(enemy, vectors[i - 1], Quaternion.identity);
+       
         }
     }
 }
